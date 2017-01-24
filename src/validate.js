b6696067e9762cf5a13e6c4bb24e948f40bbda92
Loyal Chow <loyalchow@gmail.com>
2017-01-23 18:35:10 -0800

    add validator for steps

--- src/validate.js
+++ src/validate.js
@@ -37,3 +37,37 @@ exports.runnerConfig = function(value) {
   var validator = Promise.promisify(Joi.validate);
   return validator(value, schema);
 };
+
+exports.steps = function(value) {
+  var schema = Joi.array().min(0).items(
+    Joi.object().keys({
+      type: Joi.string().valid('saveScreenshot').required(),
+      name: Joi.string().required()
+    }),
+    Joi.object().keys({
+      type: Joi.string().valid(['clickElement', 'moveTo', 'ignoreElements', 'waitForElementPresent']).required(),
+      locator: Joi.object().keys({
+        type: Joi.string().valid('css selector').required(),
+        value: Joi.string().required()
+      })
+    }),
+    Joi.object().keys({
+      type: Joi.string().valid('setElementText').required(),
+      locator: Joi.object().keys({
+        type: Joi.string().valid('css selector').required(),
+        value: Joi.string().required()
+      }),
+      text: Joi.string().required()
+    }),
+    Joi.object().keys({
+      type: Joi.string().valid('executeScript').required(),
+      code: Joi.string().required(),
+      isAsync: Joi.boolean()
+    }),
+    Joi.object().keys({
+      type: Joi.string().valid('pause').required(),
+      waitTime: Joi.number().required()
+    })
+  ).required();
+  return Joi.validate(value, schema);
+};
