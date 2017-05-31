1f348e78511c85c0caecfcc991ab0c0ff0706f71
Loyal Chow <loyalchow@gmail.com>
2017-05-30 20:43:17 -0700

    auto-add commit hash from CI env vars

--- src/ci.js
+++ src/ci.js
@@ -11,48 +11,55 @@ exports.getVars = function() {
   if (typeof env.JENKINS_URL === 'string' && env.JENKINS_URL.length > 0) {
     return {
       build: env.BUILD_NUMBER,
-      branch: env.GIT_BRANCH
+      branch: env.GIT_BRANCH,
+      commit: env.GIT_COMMIT
     };
   }
   // CircleCI
   if (env.CI === 'true' && env.CIRCLECI === 'true') {
     return {
       build: env.CIRCLE_BUILD_NUM,
-      branch: env.CIRCLE_BRANCH
+      branch: env.CIRCLE_BRANCH,
+      commit: env.CIRCLE_SHA1
     };
   }
   // Travis CI
   if (env.CI === 'true' && env.TRAVIS === 'true') {
     return {
       build: env.TRAVIS_BUILD_NUMBER,
-      branch: env.TRAVIS_BRANCH
+      branch: env.TRAVIS_BRANCH,
+      commit: env.TRAVIS_COMMIT
     };
   }
   // Codeship
   if (env.CI === 'true' && env.CI_NAME === 'codeship') {
     return {
       build: env.CI_BUILD_NUMBER,
-      branch: env.CI_BRANCH
+      branch: env.CI_BRANCH,
+      commit: env.CI_COMMIT_ID
     };
   }
   // Bitbucket
   if (env.BITBUCKET_BRANCH && env.BITBUCKET_COMMIT) {
     return {
-      branch: env.BITBUCKET_BRANCH
+      branch: env.BITBUCKET_BRANCH,
+      commit: env.BITBUCKET_COMMIT
     };
   }
   // Drone
   if (env.CI === 'true' && env.DRONE === 'true') {
     return {
       build: env.DRONE_BUILD_NUMBER,
-      branch: env.DRONE_BRANCH
+      branch: env.DRONE_BRANCH,
+      commit: env.DRONE_COMMIT
     };
   }
   // Semaphore
   if (env.CI === 'true' && env.SEMAPHORE === 'true') {
     return {
       build: env.SEMAPHORE_BUILD_NUMBER,
-      branch: env.BRANCH_NAME
+      branch: env.BRANCH_NAME,
+      commit: env.REVISION
     };
   }
   // if no matches, return empty object
