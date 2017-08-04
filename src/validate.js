4007a6c6c3b62d97617e4aa5831dddb939108ae3
Loyal Chow <loyalchow@gmail.com>
2017-08-03 23:11:47 -0700

    add beforeEachScript option

--- src/validate.js
+++ src/validate.js
@@ -104,7 +104,8 @@ var runnerSchema = Joi.object().keys({
     minLayoutPosition: Joi.number().integer().min(0)
   }),
   sauce: sauceSchema,
-  failureExitCode: Joi.number().integer().min(0).max(255).default(1)
+  failureExitCode: Joi.number().integer().min(0).max(255).default(1),
+  beforeEachScript: [Joi.func(), Joi.string()]
 }).without('resolutions', ['resolution']).with('browsers', ['sauce']).required();
 
 exports.runnerConfig = function(value) {
