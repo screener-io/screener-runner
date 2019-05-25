dfc8447d141d1f4650b1fe6a7fc798278a8fb802
Loyal Chow <loyalchow@gmail.com>
2019-05-25 12:02:17 -0700

    add clearValue and clearIgnores steps

--- src/validate.js
+++ src/validate.js
@@ -87,7 +87,7 @@ var stepsSchema = exports.stepsSchema = Joi.array().min(0).items(
     }).required()
   }),
   Joi.object().keys({
-    type: Joi.string().valid(['clickElement', 'moveTo', 'clickAndHoldElement', 'releaseElement', 'ignoreElements', 'waitForElementPresent', 'waitForElementNotPresent']).required(),
+    type: Joi.string().valid(['clickElement', 'moveTo', 'clickAndHoldElement', 'releaseElement', 'ignoreElements', 'waitForElementPresent', 'waitForElementNotPresent', 'clearElementText']).required(),
     locator: Joi.object().keys({
       type: Joi.string().valid('css selector').required(),
       value: Joi.string().required()
@@ -122,6 +122,9 @@ var stepsSchema = exports.stepsSchema = Joi.array().min(0).items(
   Joi.object().keys({
     type: Joi.string().valid('cssAnimations').required(),
     isEnabled: Joi.boolean()
+  }),
+  Joi.object().keys({
+    type: Joi.string().valid('clearIgnores').required()
   })
 );
 
