9a64adf806d4bfa1e855736e97d56d1a369ae0fa
Xinyi <xinyi.wang@saucelabs.com>
2019-11-18 17:13:18 -0800

    compate with windows

---
+++ scripts/install.js
@@ -0,0 +1,13 @@
+var sauceConnectLauncher = require("sauce-connect-launcher");
+var SAUCE_CONNECT_LAUNCHER_VERSION = '4.5.4';
+
+sauceConnectLauncher.download({
+  logger: console.log.bind(console),
+  version: SAUCE_CONNECT_LAUNCHER_VERSION,
+}, function (err) {
+  if (err) {
+    console.log("Failed to download sauce connect binary:", err);
+    console.log("sauce-connect-launcher will attempt to re-download " +
+      "next time it is run.");
+  }
+});
