5be12dc8e2d86b2c095d46df4d57344b2d6eb54f
Xinyi <xinyi.wang@saucelabs.com>
2019-11-26 10:36:03 -0800

    change api and pass all local tests

--- src/runner.js
+++ src/runner.js
@@ -132,11 +132,14 @@ exports.run = function(config) {
     // establish ngrok tunnel and resolve ngrok url
     .then(function(proxyHost) {
       if (config.tunnel) {
-        console.log('Connecting tunnel');
-        return Tunnel.connect(proxyHost || config.tunnel.host, config.tunnel.token);
-      } else {
-        return Promise.resolve();
+        console.log('Connecting ngrok tunnel');
+        return Tunnel.connect({ ngrok: { host: proxyHost || config.tunnel.host, token: config.tunnel.token }});
       }
+      if (config.sauce && config.sauce.launchSauceConnect) {
+        console.log('Connecting sauce tunnel');
+        return Tunnel.connect({ sauce: config.sauce });
+      }
+      return Promise.resolve();
     })
     // transform URL in steps and send payload to back end
     .then(function(tunnelHost) {
@@ -180,12 +183,16 @@ exports.run = function(config) {
       timer = setInterval(function() { console.log('.'); }, 120*1000);
       return api.waitForBuild(config.apiKey, config.project, config.branch, config.build).timeout(MAX_MS, 'Timeout waiting for Build');
     })
+    // disconnect tunnel and relay the response
+    .then(function(response) {
+      if (config.tunnel || (config.sauce && config.sauce.launchSauceConnect)) {
+        return Tunnel.disconnect(response);
+      }
+      return response;
+    })
+    // receive response from screener api and keep checking the build status
     .then(function(response) {
       clearInterval(timer);
-      if (config.tunnel) {
-        console.log('Disconnecting tunnel');
-        Tunnel.disconnect();
-      }
       if (response.indexOf('Build failed.') >= 0 && (config.failureExitCode !== 0 || response.indexOf('error running') >= 0)) {
         throw new Error(response);
       }
--- src/tunnel.js
+++ src/tunnel.js
@@ -1,49 +1,79 @@
-var ngrok = require('screener-ngrok');
+var ngrokLauncher = require('screener-ngrok');
 var Promise = require('bluebird');
 var url = require('url');
+var sauceConnectLauncher = require('sauce-connect-launcher');
+var sauceConnection;
 
-exports.connect = function(host, token, tries = 0) {
-  if (!token) {
-    return Promise.reject(new Error('No Tunnel Token'));
-  }
-  var href = /^https?:\/\//.test(host) ? host : 'http://' + host;
-  var urlObj = url.parse(href);
-  var options = {
-    bind_tls: true,
-    authtoken: token
-  };
-  // https:
-  if (/^https/.test(urlObj.protocol)) {
-    options.addr = 'https://' + urlObj.hostname;
-    if (urlObj.port) {
-      options.addr += ':' + urlObj.port;
-    }
-    options.host_header = 'rewrite';
-  } else { // http:
-    var port = urlObj.port || 80;
-    options.addr = urlObj.hostname + ':' + port;
-    options.host_header = urlObj.hostname;
-    if (port !== 80) {
-      // include port in host header when not default port 80
-      options.host_header += ':' + port;
-    }
-  }
-  var connect = Promise.promisify(ngrok.connect);
-  return connect(options)
-    .then(tunnelUrl => {
-      var urlObj = url.parse(tunnelUrl);
-      console.log('Connected private encrypted tunnel to ' + host + ' (' + urlObj.host.split('.')[0] + ')');
-      return urlObj.host;
+exports.connect = function({ ngrok, sauce }, tries = 0) {
+  if (sauce) {
+    return new Promise(function(resolve, reject) {
+      sauceConnection = sauceConnectLauncher({
+        username: sauce.username,
+        accessKey: sauce.accessKey,
+      }, function (err, sauceConnectProcess) {
+        sauceConnection = sauceConnectProcess;
+        if (err) {
+          reject(err);
+        }
+        console.log('Sauce Connect ready');
+        resolve();
+      });
     })
-    .catch(ex => {
-      if (tries < 2) {
-        // on error, wait and retry
-        return Promise.delay(1000).then(() =>
-          exports.connect(host, token, tries + 1)
-        );
+      .catch(ex => {
+        if (tries < 2) {
+          // on error, wait and retry
+          return Promise.delay(1000).then(() =>
+            exports.connect({ sauce }, tries + 1)
+          );
+        }
+        throw ex;
+      });
+  }
+
+  if (ngrok) {
+    var { host, token } = ngrok;
+    if (!token) {
+      return Promise.reject(new Error('No Tunnel Token'));
+    }
+    var href = /^https?:\/\//.test(host) ? host : 'http://' + host;
+    var urlObj = url.parse(href);
+    var options = {
+      bind_tls: true,
+      authtoken: token
+    };
+    // https:
+    if (/^https/.test(urlObj.protocol)) {
+      options.addr = 'https://' + urlObj.hostname;
+      if (urlObj.port) {
+        options.addr += ':' + urlObj.port;
       }
-      throw ex;
-    });
+      options.host_header = 'rewrite';
+    } else { // http:
+      var port = urlObj.port || 80;
+      options.addr = urlObj.hostname + ':' + port;
+      options.host_header = urlObj.hostname;
+      if (port !== 80) {
+        // include port in host header when not default port 80
+        options.host_header += ':' + port;
+      }
+    }
+    var connect = Promise.promisify(ngrokLauncher.connect);
+    return connect(options)
+      .then(tunnelUrl => {
+        var urlObj = url.parse(tunnelUrl);
+        console.log('Connected private encrypted tunnel to ' + host + ' (' + urlObj.host.split('.')[0] + ')');
+        return urlObj.host;
+      })
+      .catch(ex => {
+        if (tries < 2) {
+          // on error, wait and retry
+          return Promise.delay(1000).then(() =>
+            exports.connect({ ngrok }, tries + 1)
+          );
+        }
+        throw ex;
+      });
+  }
 };
 
 exports.transformUrl = function(origUrl, host, tunnelHost) {
@@ -58,6 +88,16 @@ exports.transformUrl = function(origUrl, host, tunnelHost) {
   return newUrl;
 };
 
-exports.disconnect = function() {
-  ngrok.disconnect();
+exports.disconnect = function(response) {
+  return new Promise(function(resolve) {
+    if (sauceConnection) {
+      sauceConnection.close(function() {
+        console.log('Closed Sauce Connect process');
+        resolve(response);
+      });
+    } else {
+      ngrokLauncher.disconnect();
+      resolve(response);
+    }
+  });
 };
